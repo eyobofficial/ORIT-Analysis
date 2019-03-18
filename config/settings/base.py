461f614570604ed90c059e31e9cd6897483293d9
eyob tariku <eyobtariku@gmail.com>
2019-03-18 08:10:43 +0300

    [#13] - Modify database ENGINE settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
