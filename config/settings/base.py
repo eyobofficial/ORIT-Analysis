0b1086674d1120de688a8e8d3ca8229f5d29cead
Eyob Tariku Sintaro <eyobtariku@gmail.com>
2019-03-18 09:07:07 +0300

    [#15] - Change default database to MySQL (#16)
    * [#8] - Add `.travis.yml` configuration file
    
    * [#8] - Update `requirements/base.txt` by removing unnecessary packages
    
    * [#8] - Modify `.travis.yml` configuration file
    
    * [#15] - Change default database engine to MySQL
    
    * [#15] - Remove unused packages

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
