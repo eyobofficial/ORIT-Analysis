af0f582f08603dcafc4e4252f002382d5ac88f26
Eyob Tariku Sintaro <eyobtariku@gmail.com>
2019-03-22 06:53:45 +0300

    [#24] - Create common models for later usage (#25)
    * Create the necessary migrations.
    * Register the models to `shared/admin.py`.

--- shared/admin.py
+++ shared/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Activity, Contractor, Consultant, Client, \
+    ConstructionCategory
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
+
+
+@admin.register(ConstructionCategory)
+class ConstractionCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'order')
+    search_fields = ('title', 'description')
+
+
+@admin.register(Contractor)
+class ContractorAdmin(admin.ModelAdmin):
+    list_display = ('short_name', 'long_name')
+    search_fields = ('short_name', 'long_name')
+
+
+@admin.register(Consultant)
+class ConsultantAdmin(admin.ModelAdmin):
+    list_display = ('short_name', 'long_name')
+    search_fields = ('short_name', 'long_name')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('short_name', 'long_name')
+    search_fields = ('short_name', 'long_name')
